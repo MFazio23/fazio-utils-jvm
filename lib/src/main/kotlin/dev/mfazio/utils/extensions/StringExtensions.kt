a664318596f46348fa2d26e3a0341a7a14a5f09d
Michael Fazio <mfazio23@gmail.com>
2022-06-24 21:56:31 -0500

    Add indexOfFirstOrNull and indexOfLastOrNull

--- lib/src/main/kotlin/dev/mfazio/utils/extensions/StringExtensions.kt
+++ lib/src/main/kotlin/dev/mfazio/utils/extensions/StringExtensions.kt
@@ -2,3 +2,11 @@ package dev.mfazio.utils.extensions
 
 fun String?.isNotNullOrEmpty() = !this.isNullOrEmpty()
 fun String?.isNotNullOrBlank() = !this.isNullOrBlank()
+
+fun CharSequence?.indexOfFirstOrNull(predicate: (Char) -> Boolean) = this?.indexOfFirst(predicate)?.let {
+    if (it < 0) null else it
+}
+
+fun CharSequence?.indexOfLastOrNull(predicate: (Char) -> Boolean) = this?.indexOfLast(predicate)?.let {
+    if (it < 0) null else it
+}
